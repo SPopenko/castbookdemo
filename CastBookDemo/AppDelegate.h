baccd015986759394d1d4fdd5b55e8349172f171
Anton Poluboiarynov <apolwork@gmail.com>
2012-11-12 14:51:53 +0200

    Created project for CastBook Demo

---
+++ CastBookDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CastBookDemo
+//
+//  Created by Anton Poluboiarynov on 11/12/12.
+//  Copyright (c) 2012 softserve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
