386ccaeb1c21f156103aadeae2daf1f9ecae50ae
Anton Poluboiarynov <apolwork@gmail.com>
2012-11-15 15:36:10 +0200

    implemented displaying CKCalendar on calendar tab

--- CastBookDemo/CKCalendar/CKCalendarView.m
+++ CastBookDemo/CKCalendar/CKCalendarView.m
@@ -348,7 +348,7 @@
 }
 
 - (void)setMonthShowing:(NSDate *)aMonthShowing {
-    _monthShowing = [self firstDayOfMonthContainingDate:aMonthShowing];
+    _monthShowing = [[self firstDayOfMonthContainingDate:aMonthShowing] retain];
 
     self.titleLabel.text = [self.dateFormatter stringFromDate:_monthShowing];
     [self setNeedsLayout];
